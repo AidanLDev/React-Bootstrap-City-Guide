a77cf756764f3321d13ea6baaf3fc02b78eed8d6
Aidan Lowson <aidan.lowson@contactpartners.com>
2020-05-29 17:28:39 +0100

    Adding styled component and finishing footer styles

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ export default function App() {
   return (
     <div>
       <Navbar />
+        <br />
+        <p>
+          Excepteur pariatur qui fugiat velit culpa ullamco anim tempor Lorem. Mollit sint officia ea magna deserunt incididunt id nisi occaecat eiusmod commodo reprehenderit sint eiusmod. Duis minim amet dolore Lorem nisi non ex labore enim consectetur magna voluptate Lorem reprehenderit. Amet nulla reprehenderit ea dolor exercitation officia esse velit dolor deserunt esse deserunt nisi. Sunt velit consectetur non nisi aliquip. Pariatur ea et adipisicing et irure eiusmod deserunt aliqua magna excepteur.
+        </p>
+        <p>
+          Enim aliqua cupidatat amet velit consequat. Eiusmod irure elit non id Lorem culpa sunt anim ullamco amet Lorem. Exercitation officia velit anim in. Exercitation quis occaecat nulla in tempor quis est occaecat. Labore voluptate id laborum ad exercitation culpa deserunt elit voluptate dolor do adipisicing commodo nisi. Cupidatat labore ullamco minim tempor irure. Ad sint incididunt elit cillum enim incididunt tempor consectetur exercitation est ut minim quis.
+        </p>
       <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,57 +1,81 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  ul li a {
+    color: var(--mainGrey);
+  }
+
+  ul li a:hover {
+    color: var(--mainLightGrey);
+  }
+
+  .footer-middle {
+    background: var(--mainDark);
+    padding-top: 3rem;
+    color: var(--mainWhite);
+  }
+
+  .footer-bottom {
+    padding-top: 3rem;
+    padding-bottom: 2rem;
+  }
+`;
 
 export default function Footer() {
   return (
-    <div className='main-footer'>
-      <div className='container'>
-        <div className='row'>
-          {/* Column 1 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>Lorem Ipsum</h4>
-            <ul className='list-unstyled'>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-            </ul>
+    <FooterContainer className='main-footer'>
+      <div className='footer-middle'>
+        <div className='container'>
+          <div className='row'>
+            {/* Column 1 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>Lorem Ipsum</h4>
+              <ul className='list-unstyled'>
+                <li>Lorem Ipsum</li>
+                <li>Lorem Ipsum</li>
+                <li>Lorem Ipsum</li>
+                <li>Lorem Ipsum</li>
+              </ul>
+            </div>
+            {/* Column 2 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>Lorem Ipsum</h4>
+              <ul className='list-unstyled'>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+              </ul>
+            </div>
+            {/* Column 3 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>Lorem Ipsum</h4>
+              <ul className='list-unstyled'>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+              </ul>
+            </div>
+            {/* Column 4 */}
+            <div className='col-md-3 col-sm-6'>
+              <h4>Lorem Ipsum</h4>
+              <ul className='list-unstyled'>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+                <li><a href="/">Lorem Ipsum</a></li>
+              </ul>
+            </div>
           </div>
-          {/* Column 2 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>Lorem Ipsum</h4>
-            <ul className='list-unstyled'>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-            </ul>
+          <div className='footer-bottom'>
+            <p class='text-xs-center'>
+              &copy;{new Date().getFullYear()} ~ React Bootstrap City Guide
+            </p>
           </div>
-          {/* Column 3 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>Lorem Ipsum</h4>
-            <ul className='list-unstyled'>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-            </ul>
-          </div>
-          {/* Column 4 */}
-          <div className='col-md-3 col-sm-6'>
-            <h4>Lorem Ipsum</h4>
-            <ul className='list-unstyled'>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-              <li>Lorem Ipsum</li>
-            </ul>
-          </div>
-        </div>
-        <div className='footer-bottom'>
-          <p class='text-xs-center'>
-            &copy;{new Date().getFullYear()} ~ React Bootstrap City Guide
-          </p>
         </div>
       </div>
-    </div>
+    </FooterContainer>
   );
 }
