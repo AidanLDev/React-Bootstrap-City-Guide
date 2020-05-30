6935da7119befc1734301fdd8bf21a396bcac4e8
Aidan <aidansoftwaredev@gmail.com>
2020-05-30 21:45:06 +0100

    Map through data and dispaly info cards

--- src/components/info.js
+++ src/components/info.js
@@ -1,17 +1,24 @@
-import React, { useContext } from 'react';
-import { InfoContext } from '../../context';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Info() {
-  const contextValue = useContext(InfoContext);
-  console.log(contextValue);
+export default function Info(props) {
+  const { headerTitle, img, headerSubTitle, headerText } = props.item;
   return (
     <>
       <div className='col-10 col-lg-4 mx-auto mb-5'>
-        <div className='card' style={{ width: '18rem' }}></div>
-        {/* <img src={img} alt={headerTitle} className='card-img-top' /> */}
-        <div className='card-body'>
-          {/* <h3 className='card-title text-uppercase'>{headerTitle}</h3> */}
-          {/* <h5 className='card-title'>{headerSubTitle}</h5> */}
+        <div className='card' style={{ width: '18rem' }}>
+          <img src={img} alt={headerTitle} className='card-img-top' />
+          <div className='card-body'>
+            <h3 className='card-title text-uppercase'>{headerTitle}</h3>
+            <h5 className='card-title'>{headerSubTitle}</h5>
+            <p className='card-text'>{headerText}</p>
+            <Link
+              to='/details'
+              className='btn btn-outline-primary text-uppercase'
+            >
+              More Info
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react';
 import { InfoContext } from '../../context';
-
+import Info from '../Info';
 export default function Home() {
   const contextValue = useContext(InfoContext);
   console.log(contextValue);
   return (
-    <div>
-      <h2>Home</h2>
+    <div className='container'>
+      <div className='row mt-5'>
+        {contextValue.info.map((item) => {
+          return <Info key={item.id} item={item} />;
+        })}
+      </div>
     </div>
   );
 }
